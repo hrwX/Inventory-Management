9a88dce8a3fe8763f7f312bcdf81163275a6dc75
himanshu-96 <himanshuwarekar@yahoo.com>
2018-09-07 23:08:28 +0530

    UI changes

---
+++ inventorymanagement/static/js/align.js
@@ -0,0 +1,17 @@
+// Change the selector if needed
+var $table = $('table.scroll'),
+    $bodyCells = $table.find('tbody tr:first').children(),
+    colWidth;
+
+// Adjust the width of thead cells when window resizes
+$(window).resize(function() {
+    // Get the tbody columns width array
+    colWidth = $bodyCells.map(function() {
+        return $(this).width();
+    }).get();
+    
+    // Set the width of thead columns
+    $table.find('thead tr').children().each(function(i, v) {
+        $(v).width(colWidth[i]);
+    });    
+}).resize(); // Trigger resize handler
