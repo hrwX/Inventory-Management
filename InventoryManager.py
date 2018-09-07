9f1444f0d51cb25c8e5833d265367956cea1ea4a
himanshu-96 <himanshuwarekar@yahoo.com>
2018-09-07 13:38:24 +0530

    Revert "-a"
    This reverts commit 52aa65c75e0dbb66be726791aa324c841db73ac1.

--- InventoryManager.py
+++ InventoryManager.py
@@ -1,12 +1,14 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
 from datetime import datetime
-from inventorymanagement import db, login_manager
-from flask_login import UserMixin
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '82e65b56c16931a98ff8341e28059a89'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -46,3 +48,62 @@ class ProductMovement(db.Model):
 
     def __repr__(self):
         return f"ProductMovement('{self.movement_id}', '{self.from_location}', '{self.to_location}', '{self.movement_product_quantity}', '{self.timestamp}')"
+
+
+posts=[
+    {
+        'author': 'Abc',
+        'title': 'Post 1',
+        'content': 'First post content',
+        'data_posted': 'april20, 2018'
+    },
+    {
+        'author': 'Abc',
+        'title': 'Post 2',
+        'content': 'First post content',
+        'data_posted': 'april20, 2018'
+    }, 
+    {
+        'author': 'Abc',
+        'title': 'Post 3',
+        'content': 'First post content',
+        'data_posted': 'april20, 2018'
+    }, 
+    {
+        'author': 'Abc',
+        'title': 'Post 4',
+        'content': 'First post content',
+        'data_posted': 'april20, 2018'
+    }  
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', title='Home', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f' Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
+if __name__=='__main__':
+    app.run(debug=True)
--- inventorymanagement/forms.py
+++ inventorymanagement/forms.py
@@ -1,7 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from inventorymanagement.models import User
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
@@ -9,14 +8,6 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
-    def validate_username(self, username):
-        user = User.query.filter_by(username = username.data).first()
-        if user:
-            raise ValidationError('Username Taken')
-    def validate_email(self, email):
-        user = User.query.filter_by(email = email.data).first()
-        if user:
-            raise ValidationError('Email Taken')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
