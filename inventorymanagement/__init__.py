b0b5d7578e797bd64eb62c4bbecc1e0841e1b551
himanshu-96 <himanshuwarekar@yahoo.com>
2018-09-11 11:25:22 +0530

    Switching from SQLAlchemy to MySQL

--- inventorymanagement/__init__.py
+++ inventorymanagement/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flaskext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '82e65b56c16931a98ff8341e28059a89'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+#db = SQLAlchemy(app)
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'password'
+app.config['MYSQL_DATABASE_DB'] = 'inventory_management'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
